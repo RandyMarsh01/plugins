fd12190d9c450c2128ff5823900c7b9752a7bf1e
RandyMarsh01 <stanly228322@gmail.com>
2026-01-21 17:43:52 +0300

    16

--- porntrex.js
+++ porntrex.js
@@ -8,69 +8,134 @@
         var html = $('<div class="category-full"></div>');
         var body = $('<div class="category-full__body"></div>');
         var host = 'https://www.porntrex.com';
-        
-        // Используем AllOrigins как основной, так как он прошел проверку CORS
         var proxy = 'https://api.allorigins.win/get?url=';
+        
+        // Список фильтров (Сортировка)
+        var sort_filters = [
+            { title: 'Новое', path: '/videos/' },
+            { title: 'Популярное', path: '/most-popular/' },
+            { title: 'Топ дня', path: '/top-rated/today/' },
+            { title: 'Топ месяца', path: '/top-rated/month/' }
+        ];
+
+        // Список категорий
+        var categories = [
+            { title: 'Все', path: '' },
+            { title: 'Anal', path: '/categories/anal/' },
+            { title: 'Asian', path: '/categories/asian/' },
+            { title: 'Milf', path: '/categories/milf/' },
+            { title: 'Ebony', path: '/categories/ebony/' },
+            { title: 'Group', path: '/categories/group-sex/' },
+            { title: 'Blowjob', path: '/categories/blowjob/' }
+        ];
 
         this.create = function () {
+            var _this = this;
+            
+            // 1. Создаем шапку
+            var header = $('<div class="category-full__head" style="flex-direction: column; align-items: flex-start; height: auto; padding: 1em;"></div>');
+            
+            // Кнопка поиска
+            var search_btn = $('<div class="category-full__head-item selector" style="background: #3d3d3d; margin-bottom: 0.5em; width: 100%; text-align: center;">🔍 Поиск по Porntrex</div>');
+            search_btn.on('hover:enter', function() {
+                Lampa.Input.edit({
+                    value: '',
+                    free: true,
+                    title: 'Поиск на Porntrex'
+                }, function (new_value) {
+                    if (new_value) {
+                        object.query = new_value;
+                        object.page = 1;
+                        Lampa.Activity.replace(object);
+                    }
+                });
+            });
+            header.append(search_btn);
+
+            // Ряд сортировки
+            var sort_row = $('<div style="display: flex; margin-bottom: 0.5em;"></div>');
+            sort_filters.forEach(function(f, i) {
+                var btn = $('<div class="category-full__head-item selector' + (object.filter == i && !object.category ? ' active' : '') + '">' + f.title + '</div>');
+                btn.on('hover:enter', function() {
+                    object.page = 1;
+                    object.filter = i;
+                    object.category = null;
+                    object.query = '';
+                    Lampa.Activity.replace(object);
+                });
+                sort_row.append(btn);
+            });
+            header.append(sort_row);
+
+            // Ряд категорий
+            var cat_row = $('<div style="display: flex; overflow-x: auto; width: 100%;"></div>');
+            categories.forEach(function(c, i) {
+                var active = (object.category == i);
+                var btn = $('<div class="category-full__head-item selector' + (active ? ' active' : '') + '" style="font-size: 0.8em; opacity: 0.7;">' + c.title + '</div>');
+                btn.on('hover:enter', function() {
+                    object.page = 1;
+                    object.category = i;
+                    object.filter = 0; // Сбрасываем фильтр при выборе категории
+                    object.query = '';
+                    Lampa.Activity.replace(object);
+                });
+                cat_row.append(btn);
+            });
+            header.append(cat_row);
+
+            html.append(header);
             html.append(scroll.render());
             scroll.append(body);
+            
             return html;
         };
 
         this.start = function () {
-            var _this = this;
-            setTimeout(function() {
-                if (_this.activity && _this.activity.loader) _this.activity.loader(true);
-                _this.load();
-            }, 200);
+            this.activity.loader(true);
+            this.load();
         };
 
         this.load = function () {
             var _this = this;
-            // Для AllOrigins используем главную или /videos/
-            var path = object.query ? '/search/' + encodeURIComponent(object.query) + '/' : '/videos/';
+            var path = '';
+            
+            if (object.query) {
+                path = '/search/' + encodeURIComponent(object.query) + '/';
+            } else if (object.category !== undefined && object.category !== null && object.category > 0) {
+                path = categories[object.category].path;
+            } else {
+                path = sort_filters[object.filter || 0].path;
+            }
+
             var url = proxy + encodeURIComponent(host + path + '?p=' + (object.page || 1));
 
             network.silent(url, function (json) {
-                if (_this.activity && _this.activity.loader) _this.activity.loader(false);
-                
-                // AllOrigins всегда возвращает JSON объект с полем contents
+                _this.activity.loader(false);
                 var str = (json && json.contents) ? json.contents : '';
-
                 if (str && str.indexOf('<html') !== -1) {
                     _this.parse(str);
                 } else {
-                    _this.empty('Сайт вернул пустой контент через прокси.');
+                    _this.empty('Контент не найден или прокси заблокирован.');
                 }
             }, function () {
-                if (_this.activity && _this.activity.loader) _this.activity.loader(false);
-                _this.empty('Прокси-сервер не отвечает.');
+                _this.activity.loader(false);
+                _this.empty('Ошибка сети.');
             });
-        };
-
-        this.empty = function(msg) {
-            body.empty().append('<div class="empty">' + msg + '</div>');
         };
 
         this.parse = function (str) {
             var _this = this;
             var found = 0;
-            
-            // Упрощаем HTML для парсинга
             var clean_html = str.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
             var dom = $($.parseHTML(clean_html));
-            
-            // Расширенный поиск по всем возможным классам плиток Porntrex
-            var cards = dom.find('.video-item, .item-video, .thumb-block, .p-v-thumb, .v-thumb, [class*="thumb"]');
+            var cards = dom.find('.video-item, .item-video, .thumb-block, .p-v-thumb, .v-thumb');
 
             cards.each(function () {
                 var $this = $(this);
-                // Ищем ссылку, которая ведет на страницу видео
                 var link = $this.find('a[href*="/video/"]').first();
                 var img_el = $this.find('img').first();
                 var img = img_el.attr('data-src') || img_el.attr('src') || img_el.attr('data-original');
-                var title = link.attr('title') || $this.find('.title, .name, [class*="title"]').text();
+                var title = link.attr('title') || $this.find('.title, .name').text();
                 var time = $this.find('.duration, .time, .t-length').text().trim();
 
                 if (link.attr('href') && title.trim()) {
@@ -84,16 +149,12 @@
 
                     var card = Lampa.Template.get('card', {title: card_data.title});
                     card.addClass('card--collection');
-                    
                     if (card_data.img) {
                         if (card_data.img.startsWith('//')) card_data.img = 'https:' + card_data.img;
                         card.find('.card__img').attr('src', card_data.img);
                     }
-                    
-                    if (card_data.time) {
-                        card.find('.card__view').after('<div class="card__age">' + card_data.time + '</div>');
-                    }
-                    
+                    if (card_data.time) card.find('.card__view').after('<div class="card__age">' + card_data.time + '</div>');
+
                     card.on('hover:enter', function () {
                         _this.play(card_data);
                     });
@@ -104,63 +165,48 @@
             });
 
             if (found > 0) {
+                var next = $('<div class="category-full__next selector"><span>Показать еще (Страница ' + (object.page || 1) + ')</span></div>');
+                next.on('hover:enter', function() {
+                    object.page = (object.page || 1) + 1;
+                    Lampa.Activity.replace(object);
+                });
+                body.append(next);
                 Lampa.Controller.enable('content');
             } else {
-                this.empty('Видео не найдены. Попробуйте поиск.');
+                this.empty('Тут пока ничего нет.');
             }
         };
 
         this.play = function (data) {
-            var _this = this;
-            Lampa.Noty.show('Получение видео...');
-            
-            var url = proxy + encodeURIComponent(data.url);
-            network.silent(url, function(json) {
-                var html = (json && json.contents) ? json.contents : '';
+            Lampa.Noty.show('Запрос видео...');
+            network.silent(proxy + encodeURIComponent(data.url), function(json) {
+                var html = json.contents || '';
                 var video_url = '';
-                
-                // Ищем прямую ссылку на mp4/m3u8 в коде страницы
-                var match = html.match(/"video_url":"(.*?)"/) || 
-                            html.match(/video_url:\s*'(.*?)'/) ||
-                            html.match(/source\s*src="(.*?)"/);
-
+                var match = html.match(/"video_url":"(.*?)"/) || html.match(/video_url:\s*'(.*?)'/) || html.match(/source\s*src="(.*?)"/);
                 if (match) video_url = match[1].replace(/\\/g, '');
 
                 if (video_url) {
                     if (video_url.startsWith('//')) video_url = 'https:' + video_url;
-                    
-                    Lampa.Player.play({
-                        url: video_url,
-                        title: data.title
-                    });
-                    Lampa.Player.callback(function () {
-                        Lampa.Controller.toggle('content');
-                    });
+                    Lampa.Player.play({ url: video_url, title: data.title });
+                    Lampa.Player.callback(function () { Lampa.Controller.toggle('content'); });
                 } else {
-                    Lampa.Noty.show('Не удалось найти поток. Возможно, видео удалено.');
+                    Lampa.Noty.show('Ссылка не найдена.');
                 }
             });
         };
 
+        this.empty = function(m) { body.empty().append('<div class="empty">'+m+'</div>'); };
         this.render = function () { return html; };
-        this.destroy = function () {
-            network.clear();
-            scroll.destroy();
-            html.remove();
-        };
+        this.destroy = function () { network.clear(); scroll.destroy(); html.remove(); };
     };
 
     function startPlugin() {
         Lampa.Component.add('porntrex', window.Porntrex);
-        var menu_item = $('<li class="menu__item selector" data-action="porntrex"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5V7.5L16 12L10 16.5ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z" fill="currentColor"/></svg></div><div class="menu__text">Porntrex</div></li>');
+        var menu_item = $('<li class="menu__item selector" data-action="porntrex"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 16.5V7.5L16 12L10 16.5Z" fill="white"/></svg></div><div class="menu__text">Porntrex</div></li>');
         menu_item.on('hover:enter', function () {
-            Lampa.Activity.push({ title: 'Porntrex', component: 'porntrex', page: 1 });
+            Lampa.Activity.push({ title: 'Porntrex', component: 'porntrex', page: 1, filter: 0, category: 0 });
         });
-        var list = $('.menu .menu__list').first();
-        if (list.length) {
-            list.find('[data-action="porntrex"]').remove();
-            list.append(menu_item);
-        }
+        $('.menu .menu__list').first().append(menu_item);
     }
 
     if (window.appready) startPlugin();
